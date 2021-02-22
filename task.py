4a0502c4c98587c1e71aa0803a30b988aaf8db1c
Prasad <34183552+prasadmatta-github@users.noreply.github.com>
2021-02-22 23:47:44 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,9 +1,22 @@
-import cv2, numpy as np
+import cv2, numpy as np 
+
+# cv2 module is used to reading and performing tasks on the images
+# Numpy module is used to create or perform matrix operations or manipulations
 
 def run(img):
-    gray, kernel = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY), np.ones((5,5), np.uint8) 
+    
+    # Converting BGR image into Gray scale image using cv2 module
+    # Kernel is used to perform the dilation 
+    gray, kernel = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY), np.ones((3,3), np.uint8) 
+    
+    #  Dilation will add pixels to edges of an objects which are present
+    # in the images
     img_dilation = cv2.dilate(gray, kernel, iterations=1)
-    img[img_dilation > 30] = 255 
+    # After applying image dilation only text is Black in color.  
+    # So replacing all the values of dilation image which are grater 
+    # then 85 with 255 to get Text. 
+    img[img_dilation > 85] = 255 
+    # imwrite function is used to save image locally.
     cv2.imwrite('output.jpg',img)
 
 run(cv2.imread("input.png"))
